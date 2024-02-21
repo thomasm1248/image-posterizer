dfe1767ff282f50576eaeaa8edca747220efb4a7
thomasm1248 <thomasm1248@gmail.com>
2024-02-21 14:59:03 -0700

    Layout the kmeans algorithm function

--- posterizer.py
+++ posterizer.py
@@ -1,1 +1,22 @@
 import PIL as pl
+
+# K-means algorithm
+
+def kmeans(data, k):
+    '''
+    Classify the data into k clusters.
+
+    Args:
+        data: array of datapoints, where each is an array of floats
+        k: number of clusters wanted
+
+    Returns: (means, classifications)
+        means: array of datapoints that represent the means of each cluster
+        classifications: array of integers that assigns each datapoint to a cluster
+    '''
+    # Choose some starting means
+    means = None # TODO
+    # Put all datapoints in the same group to start with
+    classifications = None # TODO
+    # Iterate until convergence is reached
+    return (means, classifications)
