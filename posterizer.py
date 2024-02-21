0b0e68c4c93ee463cc39a7b6115ffd973be80bdc
thomasm1248 <thomasm1248@gmail.com>
2024-02-21 15:24:01 -0700

    Finish kmeans algorithm

--- posterizer.py
+++ posterizer.py
@@ -25,6 +25,32 @@ def kmeans(data, k):
     changed = False
     while changed == True:
         changed = False
-        # Re-classify points and keep running total
+        # Keep a running total of the points in each cluster
+        sums = []
+        counts = []
+        for i in range(k):
+            sums.append(np.zeros((len(data[0]))))
+            counts.append(0)
+        # Re-classify points
+        for i, point in zip(range(len(data)), data):
+            # Find the closest mean
+            closestMean = means[0]
+            closestDistance = dist(closestMean, point)
+            closestJ = 0
+            for j, mean in zip(range(1, len(means)), means):
+                distance = dist(point, mean)
+                if distance < closestDistance:
+                    closestDistance = distance
+                    closestMean = mean
+                    closestJ = j
+            # Re-assign point to cluster
+            if closestJ != classifications[i]:
+                classifications[i] = closestJ
+                changed = True
+            # Add point to cluster sum
+            sums[closestJ] += point
+            counts[closestJ] += 1
         # Re-calculate means
+        for i in range(len(means)):
+            means[i] = sums[i] / counts[i]
     return (means, classifications)
